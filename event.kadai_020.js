2979cfcce05e04191a886a247cc070346dce5e16
takumi097 <igataku97614@gmail.com>
2025-01-20 19:26:50 +0900

    kadai_021課題提出

--- event.kadai_020.js
+++ event.kadai_020.js
@@ -1,9 +1,6 @@
 const Btn = document.getElementById("btn");
-const text =document.getElementById("text");
 
 Btn.addEventListener("click", () => {
-    setTimeout(() => {
     const Text = document.getElementById("text");
     Text.textContent = "ボタンをクリックしました"
-}, 2000);
 })
